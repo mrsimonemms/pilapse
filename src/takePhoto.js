57d80e26835b9c8ca7bebf0e8f1f24801ee61b26
Simon Emms <simon@simonemms.com>
2017-03-12 20:05:18 +0000

    Ensure we always return a promise

--- src/takePhoto.js
+++ src/takePhoto.js
@@ -12,92 +12,63 @@ const moment = require('moment');
 
 /* Files */
 
-module.exports = (config, sunriseSunset) => {
-  const map = {
-    startTime: 'astronomicalTwilightBegin',
-    endTime: 'astronomicalTwilightEnd'
-  };
+module.exports = (config, sunriseSunset) => Promise.resolve()
+  .then(() => {
+    const map = {
+      startTime: 'astronomicalTwilightBegin',
+      endTime: 'astronomicalTwilightEnd'
+    };
 
-  /* Are we inside the start/end times? */
-  const times = {
-    startTime: config.startTime,
-    endTime: config.endTime
-  };
+    /* Are we inside the start/end times? */
+    const times = {
+      startTime: config.startTime,
+      endTime: config.endTime
+    };
 
-  /* Ensure we have date objects for the start/end times */
-  for (const key in times) {
-    const time = times[key];
+    /* Ensure we have date objects for the start/end times */
+    for (const key in times) {
+      const time = times[key];
 
-    if (time === 'auto') {
-      /* Getting time from the sunrise/sunset */
-      const target = map[key];
+      if (time === 'auto') {
+        /* Getting time from the sunrise/sunset */
+        const target = map[key];
 
-      times[key] = sunriseSunset.get(target);
-    } else {
-      /* Converting time to Date */
-      const split = time.split(':');
+        times[key] = sunriseSunset.get(target);
+      } else {
+        /* Converting time to Date */
+        const split = time.split(':');
 
-      times[key] = moment()
-        .set({
-          hour: split[0],
-          minute: split[1] || 0,
-          second: split[2] || 0,
-          millisecond: 0
-        })
-        .toDate();
-    }
-  }
-
-  const now = Date.now();
-
-  if ((times.startTime.getTime() <= now && times.endTime.getTime() >= now) === false) {
-    /* Nothing to do */
-    return;
-  }
-
-  const savePath = [
-    config.savePath,
-    moment().get('year'),
-    moment().get('month'),
-    moment().get('date'),
-    moment().get('hour')
-  ].join(path.sep);
-
-  const fileName = `${savePath}${path.sep}${moment().get('minute')}_${now}.jpg`;
-
-  return new Promise((resolve, reject) => {
-    /* Create the path where the photos are to be stored */
-    mkdirp(savePath, (err) => {
-      if (err) {
-        reject(err);
-        return;
+        times[key] = moment()
+          .set({
+            hour: split[0],
+            minute: split[1] || 0,
+            second: split[2] || 0,
+            millisecond: 0
+          })
+          .toDate();
       }
+    }
 
-      resolve();
-    });
-  }).then(() => {
-    /* Set the options */
-    const opts = (config.raspistillOpts || []).reduce((result, opt) => {
-      result.push(opt);
+    const now = Date.now();
 
-      return result;
-    }, [
-      '-h 1536',
-      '-w 2048',
-      '-q 35',
-      '-mm matrix',
-      '-ex auto',
-      '-awb auto',
-      '-t 2000',
-      '-n'
-    ]);
+    if ((times.startTime.getTime() <= now && times.endTime.getTime() >= now) === false) {
+      /* Nothing to do */
+      return;
+    }
 
-    /* Create the command */
-    const cmd = `/opt/vc/bin/raspistill ${opts.join(' ')} -o ${fileName}`;
+    const savePath = [
+      config.savePath,
+      moment().get('year'),
+      moment().get('month'),
+      moment().get('date'),
+      moment().get('hour')
+    ].join(path.sep);
 
-    /* Execute the command to take the photo */
+    const fileName = `${savePath}${path.sep}${moment().get('minute')}_${now}.jpg`;
+
     return new Promise((resolve, reject) => {
-      exec(cmd, (err) => {
+      /* Create the path where the photos are to be stored */
+      mkdirp(savePath, (err) => {
         if (err) {
           reject(err);
           return;
@@ -105,6 +76,36 @@ module.exports = (config, sunriseSunset) => {
 
         resolve();
       });
+    }).then(() => {
+      /* Set the options */
+      const opts = (config.raspistillOpts || []).reduce((result, opt) => {
+        result.push(opt);
+
+        return result;
+      }, [
+        '-h 1536',
+        '-w 2048',
+        '-q 35',
+        '-mm matrix',
+        '-ex auto',
+        '-awb auto',
+        '-t 2000',
+        '-n'
+      ]);
+
+      /* Create the command */
+      const cmd = `/opt/vc/bin/raspistill ${opts.join(' ')} -o ${fileName}`;
+
+      /* Execute the command to take the photo */
+      return new Promise((resolve, reject) => {
+        exec(cmd, (err) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+
+          resolve();
+        });
+      });
     });
   });
-};
