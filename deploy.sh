f0616f885fa85c396332ace87fd67af3c57f23d7
Simon Emms <simon@simonemms.com>
2017-03-15 21:17:53 +0000

    Checked if it's a pull request

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,9 @@ docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
 # Build and publish
 DOCKER_COMMIT=${TRAVIS_COMMIT::8}
 DOCKER_REPO=riggerthegeek/pilapse
-DOCKER_TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+CURRENT_BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+DOCKER_TAG=`if [ "$CURRENT_BRANCH" == "master" ]; then echo "latest"; else echo $CURRENT_BRANCH; fi`
+
 docker build -f Dockerfile -t $DOCKER_REPO:$DOCKER_COMMIT .
 docker tag $DOCKER_REPO:$DOCKER_COMMIT $DOCKER_REPO:$DOCKER_TAG
 docker tag $DOCKER_REPO:$DOCKER_COMMIT $DOCKER_REPO:$TRAVIS_BUILD_NUMBER
