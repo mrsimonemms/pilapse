2119a93298f71349ad02bc10cb9d1644455211a0
Simon Emms <simon@simonemms.com>
2017-03-18 21:13:33 +0000

    Deploy script now using tag number

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-DOCKER_BUILD_NUMBER=$(git rev-parse --short HEAD)
+# This script should be run from a Raspberry Pi so uses
+# correct Node version
+#
+# Requires json to be installed globally
+
 DOCKER_REPO=riggerthegeek/pilapse
 DOCKER_COMMIT=$(uname -m)
 
@@ -10,8 +14,24 @@ then
   exit 1
 fi
 
-# Script to be run from a Raspberry Pi
+sudo rm -Rf node_modules
 npm version $1
+
+# Set the Docker version
+DOCKER_VERSION=$(json -f ./package.json version)
+
 docker build -t $DOCKER_REPO:$DOCKER_COMMIT .
-docker tag $DOCKER_REPO:$DOCKER_COMMIT $DOCKER_REPO:$DOCKER_COMMIT-$DOCKER_BUILD_NUMBER
+docker tag $DOCKER_REPO:$DOCKER_COMMIT $DOCKER_REPO:$DOCKER_COMMIT-$DOCKER_VERSION
 docker push $DOCKER_REPO
+
+docker run --name pilapse \
+  -v /var/image:/var/image \
+  -v $PWD/config.json:/opt/app/config.json \
+  -v $PWD/pilapse.sql:/opt/app/pilapse.sql \
+  --privileged \
+  --restart=always \
+  -d \
+  --log-driver=syslog \
+  --log-opt syslog-address=udp://logs.papertrailapp.com:40966 \
+  --log-opt tag=pilapse \
+  riggerthegeek/pilapse:$(uname -m)
