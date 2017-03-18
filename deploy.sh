5697289fb1a129238c4448a12da37b5cc9cf8fd5
Simon Emms <simon@simonemms.com>
2017-03-18 14:46:33 +0000

    Tagged a version when deploying

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,14 @@ DOCKER_BUILD_NUMBER=$(git rev-parse --short HEAD)
 DOCKER_REPO=riggerthegeek/pilapse
 DOCKER_COMMIT=$(uname -m)
 
+if [[ -z "$1" ]]
+then
+  echo "Please provide a version as the first argument"
+  exit 1
+fi
+
 # Script to be run from a Raspberry Pi
+npm version $1
 docker build -t $DOCKER_REPO:$DOCKER_COMMIT .
 docker tag $DOCKER_REPO:$DOCKER_COMMIT $DOCKER_REPO:$DOCKER_COMMIT-$DOCKER_BUILD_NUMBER
 docker push $DOCKER_REPO
